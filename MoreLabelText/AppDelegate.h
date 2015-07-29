755c0b3d95ec4789c9cfbc5e0a9651393b4d8a42
郇建忠 <huanjianzhong@lakala.com>
2015-07-29 09:46:06 +0800

    Initial Commit

---
+++ MoreLabelText/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MoreLabelText
+//
+//  Created by lakala on 15/7/29.
+//  Copyright (c) 2015年 lakala. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MoreLabelText/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  MoreLabelText
+//
+//  Created by lakala on 15/7/29.
+//  Copyright (c) 2015年 lakala. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
